bc1d9fec5e8c97860acbd37fff7ffec8a9d35ffa
Faiz Murtadha <faizmrtdha99@gmail.com>
2022-08-27 12:55:10 +0700

    add some item

--- src/components/Card/CardNotes.jsx
+++ src/components/Card/CardNotes.jsx
@@ -1,10 +1,19 @@
 import React from "react"
+import { showFormattedDate } from "../../utils"
 
 const CardNotes = ({ title, body, createdAt }) => {
+  const date = +new Date()
+
   return (
     <div className="note-item">
       <div className="note-item__content">
-        <div className="note-item__title">{title}</div>
+        <h3 className="note-item__title">{title}</h3>
+        <p className="note-item__date">{showFormattedDate(date)}</p>
+        <p className="note-item__body">{body}</p>
+      </div>
+      <div className="note-item__action">
+        <button className="note-item__delete-button">Delete</button>
+        <button className="note-item__archive-button">Arsipkan</button>
       </div>
     </div>
   )
